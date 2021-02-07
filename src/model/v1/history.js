ceada1b570bc092581fb69927643148f3b547096
maulanarifai114 <maulanarifai114@gmail.com>
2021-02-07 14:29:43 +0700

    change query at history

--- src/model/v1/history.js
+++ src/model/v1/history.js
@@ -16,7 +16,26 @@ const history = {
       INNER JOIN users AS sender ON trans.senderId = sender.id
       WHERE trans.senderId = '${id}' 
       OR trans.receiverId = '${id}'
-      ORDER BY trans.dateCreate DESC
+      ORDER BY trans.dateCreate DESC LIMIT 7
+      `
+    );
+  },
+  newHistoryLimit: (id) => {
+    return actionQuery(
+      `
+      SELECT trans.*,
+      sender.firstName AS senderFirstName,
+      sender.lastName AS senderLastName,
+      sender.photo AS senderPhoto,
+      receiver.firstName AS receiverFirstName,
+      receiver.lastName AS receiverLastName,
+      receiver.photo AS receiverPhoto
+      FROM transactions AS trans
+      INNER JOIN users AS receiver ON trans.receiverId = receiver.id
+      INNER JOIN users AS sender ON trans.senderId = sender.id
+      WHERE (trans.senderId = '${id}' AND trans.dateCreate < '${date}')
+      OR (trans.receiverId = '${id}' AND trans.dateCreate < '${date}')
+      ORDER BY trans.dateCreate DESC LIMIT 7
       `
     );
   },
@@ -35,7 +54,26 @@ const history = {
       INNER JOIN users AS sender ON trans.senderId = sender.id
       WHERE trans.senderId = '${id}' 
       OR trans.receiverId = '${id}'
-      ORDER BY trans.dateCreate ASC
+      ORDER BY trans.dateCreate ASC LIMIT 7
+      `
+    );
+  },
+  oldHistoryLimit: (id) => {
+    return actionQuery(
+      `
+      SELECT trans.*,
+      sender.firstName AS senderFirstName,
+      sender.lastName AS senderLastName,
+      sender.photo AS senderPhoto,
+      receiver.firstName AS receiverFirstName,
+      receiver.lastName AS receiverLastName,
+      receiver.photo AS receiverPhoto
+      FROM transactions AS trans
+      INNER JOIN users AS receiver ON trans.receiverId = receiver.id
+      INNER JOIN users AS sender ON trans.senderId = sender.id
+      WHERE (trans.senderId = '${id}' AND trans.dateCreate > '${date}')
+      OR (trans.receiverId = '${id}' AND trans.dateCreate > '${date}')
+      ORDER BY trans.dateCreate ASC LIMIT 7
       `
     );
   },
