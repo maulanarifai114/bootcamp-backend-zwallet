88cdacf7c2d8d584193828f6970f3aefb4c32711
maulanarifai114 <maulanarifai114@gmail.com>
2021-02-08 11:56:00 +0700

    fixing bug at get all user exc admin

--- src/controller/v1/admin/getAllUserExAdmin.js
+++ src/controller/v1/admin/getAllUserExAdmin.js
@@ -1,10 +1,24 @@
 const { getAllUserExAdmin } = require("../../../model/v1/admin");
 const helper = require("../../../helper/v1/help");
+const moment = require("moment");
 
 exports.getAllUserExAdmin = (req, res) => {
+  const allData = [];
   if (req.id === process.env.ADMIN) {
     getAllUserExAdmin()
-      .then((result) => helper.response(res, result, 200, null))
+      .then((result) => {
+        result.forEach((el) => {
+          const data = {
+            id: el.id,
+            name: el.name,
+            photo: el.photo,
+            phone: el.phone,
+            date: moment(el.date).format().split("+07:00")[0],
+          }
+          allData.push(data)
+        })
+        helper.response(res, allData, 200, null)
+      })
       .catch((err) => helper.reject(res, null, 404, err));
   } else {
     helper.reject(res, null, 401, "You are not an admin");
